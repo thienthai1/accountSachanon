ad2fb50ad4bfaf37f18a9f38f28f2b4d9c4eaacd
jarvis <surkessone@gmail.com>
2019-05-06 00:10:01 +0700

    add database

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <v-app>
     <router-view></router-view>
+    <h1>{{ ng }}</h1>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld'
+import firebase from './firebase'
 
 export default {
   name: 'App',
@@ -14,8 +16,14 @@ export default {
   },
   data () {
     return {
-      //
+      ng: test()
     }
+  },
+  created() {
+    var readRef = firebase.database().ref('members/thienthai/password');
+    readRef.on('value', function(snapshot) {
+      alert(snapshot.val())
+    });
   }
 }
 </script>
