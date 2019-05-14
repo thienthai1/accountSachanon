74cc3b27309505e7e20616d27be0b2855acac39e
jarvis <surkessone@gmail.com>
2019-05-15 03:36:16 +0700

    add table and sort

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,16 +4,30 @@
       <v-divider class="my-3"></v-divider>
       <v-data-table
         :headers="headers"
-        :items="desserts"
+        :items="myData"
         class="elevation-1"
       >
         <template v-slot:items="props">
-          <td>{{ props.item.name }}</td>
-          <td class="text-xs-left">{{ props.item.calories }}</td>
-          <td class="text-xs-left">{{ props.item.fat }}</td>
-          <td class="text-xs-left">{{ props.item.carbs }}</td>
-          <td class="text-xs-left">{{ props.item.protein }}</td>
-          <td class="text-xs-left">{{ props.item.iron }}</td>
+          <td>{{ props.item.date }}</td>
+          <td class="text-xs-left">{{ props.item.time }}</td>
+          <td class="text-xs-left">{{ props.item.name }}</td>
+          <td class="text-xs-left">{{ props.item.price }}</td>
+          <td class="text-xs-left">{{ props.item.remark }}</td>
+          <td class="text-xs-left">
+            <v-icon
+              small
+              class="mr-2"
+              @click="editItem(item)"
+            >
+              edit
+            </v-icon>
+            <v-icon
+              small
+              @click="deleteItem(item)"
+            >
+              delete
+            </v-icon>
+          </td>
         </template>
       </v-data-table>
     </v-container>
@@ -24,109 +38,43 @@ import firebase from '../firebase'
   export default {
   name: 'Home',
   beforeCreate() {
-    var readRef = firebase.database().ref("Outin")
-    readRef.once('value',function(snapshot){
-      snapshot.forEach(function(childSnapshot) {
-        console.log(childSnapshot.key + "," + childSnapshot.val())
-      });
-    });
   },
   data () {
     return {
+       myData: [],
        headers: [
           {
             text: 'วันที่',
             align: 'left',
-            sortable: true,
-            value: 'name'
+            sortable: false,
+            value: 'date',
           },
-          { text: 'รายการ', value: 'calories' },
-        ],
-        desserts: [
-          {
-            name: 'Frozen Yogurt',
-            calories: 159,
-            fat: 6.0,
-            carbs: 24,
-            protein: 4.0,
-            iron: '1%'
-          },
-          {
-            name: 'Ice cream sandwich',
-            calories: 237,
-            fat: 9.0,
-            carbs: 37,
-            protein: 4.3,
-            iron: '1%'
-          },
-          {
-            name: 'Eclair',
-            calories: 262,
-            fat: 16.0,
-            carbs: 23,
-            protein: 6.0,
-            iron: '7%'
-          },
-          {
-            name: 'Cupcake',
-            calories: 305,
-            fat: 3.7,
-            carbs: 67,
-            protein: 4.3,
-            iron: '8%'
-          },
-          {
-            name: 'Gingerbread',
-            calories: 356,
-            fat: 16.0,
-            carbs: 49,
-            protein: 3.9,
-            iron: '16%'
-          },
-          {
-            name: 'Jelly bean',
-            calories: 375,
-            fat: 0.0,
-            carbs: 94,
-            protein: 0.0,
-            iron: '0%'
-          },
-          {
-            name: 'Lollipop',
-            calories: 392,
-            fat: 0.2,
-            carbs: 98,
-            protein: 0,
-            iron: '2%'
-          },
-          {
-            name: 'Honeycomb',
-            calories: 408,
-            fat: 3.2,
-            carbs: 87,
-            protein: 6.5,
-            iron: '45%'
-          },
-          {
-            name: 'Donut',
-            calories: 452,
-            fat: 25.0,
-            carbs: 51,
-            protein: 4.9,
-            iron: '22%'
-          },
-          {
-            name: 'KitKat',
-            calories: 518,
-            fat: 26.0,
-            carbs: 65,
-            protein: 7,
-            iron: '6%'
-          }
+          { text: 'เวลา', value: 'time', sortable: false },
+          { text: 'รายการ', value: 'name', sortable: false },
+          { text: 'ราคา', value: 'price', sortable: false },
+          { text: 'หมายเหตุ', value: 'remark', sortable: false },
+          { text: ''}
         ]
     }
   },
-  methods: {
-  }
+  mounted: function () {
+      var readRef = firebase.database().ref("Outin")
+      var items = []
+      var sortItems = []
+      readRef.on('value', (snapshot) => {
+        items = []
+        snapshot.forEach( (childSnapshot) => {
+          items.push(childSnapshot.val())
+          this.myData = items
+        });
+        sortItems = []
+        var j = items.length-1
+        for(;j>=0;j--){
+          sortItems.push(items[j])
+        }
+        this.myData = sortItems
+      });
+      // this.myData = sortItems 
+  },
 }
 </script>
